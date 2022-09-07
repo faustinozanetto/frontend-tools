3952436ca3b14a98dc5f06f0a27590e208e2b9eb
Faustino Zanetto <faustizanetto@gmail.com>
2022-09-07 17:36:16 -0300

    Added basic shadows and card.

--- src/modules/tools/shadow-collection/components/card/shadow-collection-card.tsx
+++ src/modules/tools/shadow-collection/components/card/shadow-collection-card.tsx
@@ -1,3 +1,5 @@
+import { Transition } from '@headlessui/react';
+import { motion } from 'framer-motion';
 import React from 'react';
 import { ShadowCollectionData } from '../../types/shadow-collection.types';
 
@@ -8,7 +10,30 @@ interface IShadowCollectionCardProps {
 const ShadowCollectionCard: React.FC<IShadowCollectionCardProps> = (props) => {
   const { shadow } = props;
 
-  return <div className="flex p-4"></div>;
+  return (
+    <motion.div
+      variants={{
+        visible: {
+          opacity: 1,
+          y: 0,
+        },
+        hidden: {
+          opacity: 0,
+          y: -15,
+        },
+      }}
+      initial="hidden"
+      animate="visible"
+      whileHover="hover"
+      transition={{
+        duration: 0.15,
+        delay: Number(shadow.id) * 0.1,
+        staggerChildren: 0.15,
+      }}
+      className="bg-white rounded-lg border border-gray-200 h-[200px] w-[200px]"
+      style={{ boxShadow: shadow.shadowStyle }}
+    ></motion.div>
+  );
 };
 
 export default ShadowCollectionCard;
